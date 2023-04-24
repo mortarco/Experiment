067256fa98e45fb019dc90ae10fa636df259f539
Mortarco <mortarco@denasu.com>
2023-04-24 15:23:07 +0900

    test

--- Sample/src/test/java/com/denasu/sample/jetty/SampleJettyApplicationTests.java
+++ Sample/src/test/java/com/denasu/sample/jetty/SampleJettyApplicationTests.java
@@ -1,5 +1,41 @@
 package com.denasu.sample.jetty;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
 public class SampleJettyApplicationTests
 {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Test
+    public void test() {
+        String good = restTemplate.getForObject("/", String.class);
+        assertEquals(good, "Good");
+    }
+    @SpringBootConfiguration
+    public static class TestConfig {
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate() {
+                @SuppressWarnings("unchecked")
+				@Override
+                public <T> T getForObject(String url, Class<T> responseType, Object... uriVariables) throws RestClientException {
+                    if (responseType == String.class) {
+                        return (T) "Good";
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
+                }
+            };
+        }
+    }
 }
